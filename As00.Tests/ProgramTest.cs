8acc0a558f4b8cf3765c9cafb57aeaaefb4ae17c
rakul <rakt@itu.dk>
2022-09-02 15:14:08 +0200

    Tested and implemented IsLeapYear

---
+++ As00.Tests/ProgramTest.cs
@@ -0,0 +1,85 @@
+namespace As00.Tests;
+
+public class ProgramTest
+{
+    [Fact]
+    public void Is_Leap_Year_Eval_True_On_Mod4()
+    {
+        // Arrange
+        var sut = new Program();
+
+        // Act
+        bool result = sut.IsLeapYear(16);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Is_Leap_Year_Eval_False_On_Mod4()
+    {
+        // Arrange
+        var sut = new Program();
+
+        // Act
+        bool result = sut.IsLeapYear(81);
+
+        // Assert
+        result.Should().BeFalse();
+
+    }
+    
+    [Fact]
+    public void Is_Leap_Year_Eval_True_On_Mod4_And_100()
+    {
+        // Arrange
+        var sut = new Program();
+
+        // Act
+        bool result = sut.IsLeapYear(400);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Is_Leap_Year_Eval_False_On_Mod4_And_100()
+    {
+        // Arrange
+        var sut = new Program();
+
+        // Act
+        bool result = sut.IsLeapYear(281);
+
+        // Assert
+        result.Should().BeFalse();
+
+    }
+
+    [Fact]
+    public void Is_Leap_Year_Eval_True_On_Mod4_And_100_And_400()
+    {
+        // Arrange
+        var sut = new Program();
+
+        // Act
+        bool result = sut.IsLeapYear(2000);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Is_Leap_Year_Eval_False_On_Mod4_And_100_And_400()
+    {
+        // Arrange
+        var sut = new Program();
+
+        // Act
+        bool result = sut.IsLeapYear(1700);
+
+        // Assert
+        result.Should().BeFalse();
+
+    }
+}
